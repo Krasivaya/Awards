b05f5c0ec4d61c711bfacd8f3348e5cc679a60d5
Krasivaya <semwagacarine@gmail.com>
2019-11-02 17:04:43 +0200

    ch(app): Add index view function

--- awardapp/views.py
+++ awardapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import datetime as dt
 
-# Create your views here.
+def index(request):
+    date = dt.date.today()    
+
+    return render(request, 'index.html', {"date": date, "projects":projects})
