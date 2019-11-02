7b0a66e0fa1fe3949fdc9ee5fe8788e9089091db
Krasivaya <semwagacarine@gmail.com>
2019-11-02 17:35:05 +0200

    ch(app): Add register view function

--- awardapp/views.py
+++ awardapp/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.contrib import messages
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer, ProjectSerializer
+
+# Create your views here.
 
 def index(request):
-    date = dt.date.today()    
+    date = dt.date.today()
+    projects = Projects.get_projects()
+    
 
     return render(request, 'index.html', {"date": date, "projects":projects})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('/')
+        
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/registration_form.html', {'form':form})
+    
+    
