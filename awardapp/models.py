07573676b6e85ea84ca39eac387e7beebff0ac60
Krasivaya <semwagacarine@gmail.com>
2019-11-02 17:15:53 +0200

    ch(app): add project model

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from star_ratings.models import Rating
 
         
 class Profile(models.Model):
@@ -7,4 +9,13 @@ class Profile(models.Model):
     bio = models.TextField(blank=True)
     photo = models.ImageField(upload_to = 'profile/', blank=True, default='profile/default.png')
 
-      
+    
+class Projects(models.Model):
+    project_title = models.CharField(max_length=255)
+    project_image = models.ImageField(upload_to = 'images/', default='images/default.png')
+    project_description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    link = models.URLField()
+    country = CountryField(blank_label='(select country)', default='RW')
