ae0797024c803c0504e485d407afbcd75dc5743a
Krasivaya <semwagacarine@gmail.com>
2019-11-02 17:43:21 +0200

    ch(app): Get projects

--- awardapp/views.py
+++ awardapp/views.py
@@ -50,3 +50,14 @@ def search_projects(request):
         return render(request, 'search.html', {"message": message})
 
 
+def get_project(request, id):
+
+    try:
+        project = Projects.objects.get(pk = id)
+        
+    except ObjectDoesNotExist:
+        raise Http404()
+    
+    
+    return render(request, "projects.html", {"project":project})
+  
