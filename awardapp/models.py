115f676d7b98c85ad04e05c5727cb44faa28b8ce
Krasivaya <semwagacarine@gmail.com>
2019-11-02 17:06:52 +0200

    ch(app): Add profile model

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+        
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = 'profile/', blank=True, default='profile/default.png')
+
+      
